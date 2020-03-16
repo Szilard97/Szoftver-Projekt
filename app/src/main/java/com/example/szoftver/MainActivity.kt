5f3e4b635eac7d0c47c7233534dcfcedeb16f2be
Szilard97 <szilardkristo@yahoo.com>
2020-03-16 12:44:23 +0200

    Fixed problem

--- app/src/main/java/com/example/szoftver/MainActivity.kt
+++ app/src/main/java/com/example/szoftver/MainActivity.kt
@@ -6,10 +6,9 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
 
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //itt vagyok
-        int szam = 5;
+        setContentView(R.layout.activity_main)
+
     }
 }
